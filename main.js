26e9af4506c81d6ebf04b9bf8989eb9756854105
sparkfu <maxbillerbeck777@gmail.com>
2023-02-12 20:13:05 +0100

    added

--- main.js
+++ main.js
@@ -13,5 +13,6 @@ addToDoButton.addEventListener('click', function(){
     })
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.appendChild(paragraph);
+        
     })
 })
