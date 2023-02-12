274fe49837698371de392a11dcff1cddf8241e1f
sparkfu <maxbillerbeck777@gmail.com>
2023-02-12 20:09:16 +0100

    added

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ addToDoButton.addEventListener('click', function(){
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
     paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
+        paragraph.style.textDecoration = "line-through"
     })
     paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
+        toDoContainer.appendChild(paragraph);
     })
 })
