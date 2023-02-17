6e88b2ae58fdac64ff3a6717339daac57321fbd4
sparkfu <maxbillerbeck777@gmail.com>
2023-02-17 20:56:13 +0100

    added

---
+++ script.js
@@ -0,0 +1,54 @@
+let optionButtons = document.querySelectorAll("option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.getElementById(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+//List of fonts
+let fontList = ["Arial","Verdana","Times New Roman","Garamond","Georgia","Courier New","cursive"];
+
+//Initial Settings
+const initializer = () => {
+    highlighter(alignButtons, true);
+    highlighter(spacingButtons, true);
+    highlighter(formatButtons, true);
+    highlighter(scriptButtons, true);
+};
+
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            if(needsRemoval){
+                let alreadyActive = false;
+
+                if(button.classList.contains("active")) {
+                    alreadyActive = true;
+                }
+
+                highlighterRemover(className);
+                if(!alreadyActive) {
+                    button.classList.add("active")
+                }
+            }
+            else{
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+const highlighterRemover = (className) => {
+    className.forEach((button) => {
+        button.classList.remove("active");
+    });
+}
+
+window.onload = initializer();
+
+
+
