57afba1607a200cf977e20a542177e2085f0c06e
sparkfu <maxbillerbeck777@gmail.com>
2023-02-17 21:42:36 +0100

    added

--- script.js
+++ script.js
@@ -18,6 +18,20 @@ const initializer = () => {
     highlighter(spacingButtons, true);
     highlighter(formatButtons, true);
     highlighter(scriptButtons, true);
+
+    fontList.map(value => {
+        let option = document.createElement("option");
+        option.value = value;
+        option.innerHTML = value;
+        fontName.appendChild(option);
+    });
+
+    for(let i = 1; i <= 7; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.innerHTML = i;
+        fontSizeRef.appendChild(option);
+    }
 };
 
 const highlighter = (className, needsRemoval) => {
