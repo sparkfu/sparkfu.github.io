f7b3cd874a87aed4ffde0a2b6f7d3c5155b08d31
sparkfu <maxbillerbeck777@gmail.com>
2023-02-18 14:03:54 +0100

    added

--- script3.js
+++ script3.js
@@ -1,6 +1,14 @@
 function saveName() {
-    var name = document.getElementById("name").value;
-    var file = new File([name], "names.txt", {type: "text/plain;charset=utf-8"});
-    saveAs(file);
-  }
-  
+  var name = document.getElementById("name").value;
+  var file = new File([name], "names.txt", {type: "text/plain;charset=utf-8"});
+  saveAs(file);
+}
+
+// Warten, bis das DOM vollständig geladen ist
+document.addEventListener('DOMContentLoaded', function() {
+    // Den Button auswählen und einen Event-Listener hinzufügen
+    document.getElementById('save-btn').addEventListener('click', function() {
+      // Die Funktion saveName() aufrufen
+      saveName();
+    });
+  });
